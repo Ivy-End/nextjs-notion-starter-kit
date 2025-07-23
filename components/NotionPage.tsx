1772470b85e1b5c07c72bbfa14c221ac2e7ba9cb
Ivy-End <prc.wkai@gmail.com>
2025-07-23 11:26:40 +0800

    fix sorting

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,37 +1,32 @@
-import cs from 'classnames'
-import dynamic from 'next/dynamic'
-import Image from 'next/legacy/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { type PageBlock } from 'notion-types'
-import { formatDate, getBlockTitle, getPageProperty } from 'notion-utils'
-import * as React from 'react'
-import BodyClassName from 'react-body-classname'
-import {
-  type NotionComponents,
-  NotionRenderer,
-  useNotionContext
-} from 'react-notion-x'
-import { EmbeddedTweet, TweetNotFound, TweetSkeleton } from 'react-tweet'
-import { useSearchParam } from 'react-use'
+import * as React from 'react';
+import BodyClassName from 'react-body-classname';
+import cs from 'classnames';
+import dynamic from 'next/dynamic';
+import Image from 'next/legacy/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { type PageBlock } from 'notion-types';
+import { formatDate, getBlockTitle, getPageProperty } from 'notion-utils';
+import { EmbeddedTweet, TweetNotFound, TweetSkeleton } from 'react-tweet';
+import { useSearchParam } from 'react-use';
 
-import type * as types from '@/lib/types'
-import * as config from '@/lib/config'
-import { mapImageUrl } from '@/lib/map-image-url'
-import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
-import { searchNotion } from '@/lib/search-notion'
-import { useDarkMode } from '@/lib/use-dark-mode'
+import * as config from '@/lib/config';
+import { mapImageUrl } from '@/lib/map-image-url';
+import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url';
+import { searchNotion } from '@/lib/search-notion';
+import { useDarkMode } from '@/lib/use-dark-mode';
+import type * as types from '@/lib/types';
 
-import { Footer } from './Footer'
-// import { GitHubShareButton } from './GitHubShareButton'
-import GiscusDiscussion from './GiscusDiscussion'
-import PasswordGate from './PasswordGate'
-import { Loading } from './Loading'
-import { NotionPageHeader } from './NotionPageHeader'
-import { Page404 } from './Page404'
-import { PageAside } from './PageAside'
-import { PageHead } from './PageHead'
-import styles from './styles.module.css'
+import { Footer } from './Footer';
+import GiscusDiscussion from './GiscusDiscussion';
+import PasswordGate from './PasswordGate';
+import { Loading } from './Loading';
+import { NotionPageHeader } from './NotionPageHeader';
+import { Page404 } from './Page404';
+import { PageAside } from './PageAside';
+import { PageHead } from './PageHead';
+import styles from './styles.module.css';
+// import { GitHubShareButton } from './GitHubShareButton';
 
 // -----------------------------------------------------------------------------
 // dynamic imports for optional components
